0b4c5024d3d3bb0dd087acd9aa51e66ff635d877
ulises801 <ulises-801@outlook.es>
2022-02-24 17:41:20 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+/*
+Verbos HTTP
+GET
+POST
+PATCH 
+PUT
+DELETE
+*/
+app.get("/", (req, res, next)=>{
+    res.status(200);
+    res.send("Bienvenido");
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is running")
+});

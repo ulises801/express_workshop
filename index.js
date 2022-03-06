bfe2fd87284ff2fb944042891bf3401a5b9a30ca
ulises801 <ulises-801@outlook.es>
2022-03-06 16:02:04 -0600

    NodeJS 00 Terminando de configurar el proyecto y obteniendo parámetros de la URL

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
 
-/*
-Verbos HTTP
-GET
-POST
-PATCH 
-PUT
-DELETE
-*/
 app.get("/", (req, res, next)=>{
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenido al servidor");
 });
 
-app.listen(3000, ()=>{
-    console.log("Server is running")
+app.get("/:name", (req, res, next)=>{
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola, " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server is running...")
 });

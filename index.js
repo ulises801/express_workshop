a07a3847d3700956b8f1659027996e91d17acdcc
ulises801 <ulises-801@outlook.es>
2022-03-07 16:25:16 -0600

    NodeJS 04 POST, Body, Parser y Postman

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const { pokemon } = require('./pokedex.json');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res, next)=>{
     return res.status(200).send("Bienvenido al Pokedex");
+});
+
+app.post("/pokemon/", (req, res, next) => {
+    return res.status(200).send(req.body);
 });
 
 app.get("/pokemon", (req, res, next)=>{
@@ -12,19 +19,22 @@ app.get("/pokemon", (req, res, next)=>{
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
     const id = req.params.id - 1;
-    (id >= 0 && id <= 150) ?
-        res.status(200).send(pokemon[req.params.id - 1]):
-        res.status(404).send("Pokemon no encontrado.");
+    if(id >= 0 && id <= 150) {
+        res.status(200).send(pokemon[req.params.id - 1])
+    }
+    res.status(404).send("Pokemon no encontrado.");
 });
 
 app.get('/pokemon/:name([A-Za-z]+)', (req, res, next)=>{
     const name = req.params.name;
+
     const pk = pokemon.filter((p)=>{
-        return (p.name.toUpperCase()==name.toUpperCase()) ? p : null;
+        return (p.name.toUpperCase()==name.toUpperCase()) && p;
     });
 
-    (pk.length > 0) ? 
-    res.status (200).send(pk) : 
+    if (pk.length > 0) {
+        res.status (200).send(pk)
+    }
     res.status(404).send("Pokemon no encontrado");
 });
 

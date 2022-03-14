89c8005bba7221937201788476029be0e9df6d40
ulises801 <ulises-801@outlook.es>
2022-03-14 13:34:22 -0600

    NodeJS 07 cambiando body parser

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next)=>{
-    return res.status(200).send("Bienvenido al Pokedex");
+    return res.status(200).json({ code: 1, message: "Bienvenido al pokedex"});
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrada"});
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...")

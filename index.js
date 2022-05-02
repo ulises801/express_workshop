d15eaeef80d3d145751710195f1e8f79b4e6ccb3
ulises801 <ulises-801@outlook.es>
2022-05-01 21:16:42 -0500

    changed index

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) =>{
-    res.status(200);
-    res.send("Hola mundo");
-});
+app.get("/", index);
 app.use("/user", user);
 app.use(auth);
 app.use("/pokemon", pokemon);
